31894a99db98272ee0acbcad1d96dce33171266b
Aaron Kessler <kessler.aaron@gmail.com>
2021-03-13 14:38:31 -0500

    add dynamic data to drive html results

--- vuetestproj1/app.js
+++ vuetestproj1/app.js
@@ -1,6 +1,12 @@
 const app = Vue.createApp({
     //data, functions
-    
+    data() {
+        return {
+            title: 'The Final Empire',
+            author: 'Brandon Sanderson',
+            age: 45
+        }
+    }
 
 
 })
