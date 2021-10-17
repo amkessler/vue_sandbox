e0a37a4fe30a3d11aec8bb50ecab5e6223f3f483
Aaron Kessler <kessler.aaron@gmail.com>
2021-10-17 16:29:19 -0400

    add dummy text

--- router-project/src/views/About.vue
+++ router-project/src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>Sit laboriosam excepturi itaque, sunt harum porro quae non. Aperiam sint maxime dolores nihil. Ducimus, deserunt quos!</p>
   </div>
 </template>
--- router-project/src/views/Contact.vue
+++ router-project/src/views/Contact.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="contact">
     <h1>This is a contact page</h1>
+    <p>Aperiam sint maxime dolores nihil. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit laboriosam excepturi itaque, sunt harum porro quae non. Aperiam sint maxime dolores nihil. Ducimus, deserunt quos!</p>
   </div>
 </template>
